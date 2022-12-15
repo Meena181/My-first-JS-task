8728cb6b3539f2b5192cf6a9e46f80217c5ac55e
Meena181 <114894441+Meena181@users.noreply.github.com>
2022-12-15 14:02:39 +0530

    The JS task
    A simple web page with Java script functions that alters the style display and color properties

---
+++ script.js
@@ -0,0 +1,10 @@
+var box= document.querySelector(".box");
+function hide(){
+    box.style="display:none;";
+}
+function show(){
+    box.style="";
+}
+function showhide(option){
+	box.style=option;
+}
